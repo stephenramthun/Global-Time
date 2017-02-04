8c6f7f1734f9032d62baf19aa6f67bf0a26d1262
Stephen Ramthun <stephenramthun@gmail.com>
2017-02-03 17:19:14 -0800

    Added basic status bar clock

--- Global Time/AppDelegate.m
+++ Global Time/AppDelegate.m
@@ -11,18 +11,44 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+
+@property (nonatomic, strong) NSStatusItem *statusItem;
+@property (nonatomic, strong) NSMenu *menu;
+@property (nonatomic, strong) NSTimer *timer;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    
+    NSStatusBar *bar = [NSStatusBar systemStatusBar];
+    self.menu = [[NSMenu alloc] init];
+    self.statusItem = [self getClockItemWithStatusBar:bar andMenu:self.menu];
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateStyle = NSDateFormatterNoStyle;
+    dateFormatter.timeStyle = NSDateFormatterShortStyle;
+    dateFormatter.locale = [NSLocale currentLocale];
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 repeats:YES block:^(NSTimer * _Nonnull timer){
+        NSDate *currentDate = [NSDate date];
+        [self.statusItem setTitle:[dateFormatter stringFromDate:currentDate]];
+    }];
 }
 
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+    
+    
 }
 
+- (NSStatusItem *)getClockItemWithStatusBar:(NSStatusBar *)bar andMenu:(NSMenu *)menu {
+    NSStatusItem *clockItem = [bar statusItemWithLength:NSVariableStatusItemLength];
+    [clockItem setTitle:@""];
+    [clockItem setHighlightMode:YES];
+    [clockItem setMenu:menu];
+    return clockItem;
+}
 
 @end
