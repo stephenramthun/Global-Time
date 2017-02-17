740976e67430f6239305289331162f3e7f6fe748
Stephen Ramthun <stephenramthun@gmail.com>
2017-02-17 13:49:14 -0800

    Comments

--- Global Time/AppDelegate.h
+++ Global Time/AppDelegate.h
@@ -5,6 +5,9 @@
 //  Created by Stephen Ramthun on 03/02/2017.
 //  Copyright © 2017 Stephen Ramthun. All rights reserved.
 //
+//  Global Time is a simple world clock application for macOS, letting the
+//  user know what time it is in any city in the world at a glance.
+//  Time is shown either in a separate window, the system status bar, or both.
 
 #import <Cocoa/Cocoa.h>
 
